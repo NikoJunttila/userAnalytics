b33d3ff319e0cb28e1afc13c4d684c07002d01cd
Niko Junttila <Nikosamulijunttila@gmail.com>
2024-01-25 15:27:25 +0200

    update 2

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -12,6 +12,39 @@ import (
 	"github.com/google/uuid"
 )
 
+const createPasswordReset = `-- name: CreatePasswordReset :one
+INSERT INTO password_resets(id,expiration,email,valid,token)
+VALUES($1,$2,$3,$4,$5)
+RETURNING id, expiration, email, valid, token
+`
+
+type CreatePasswordResetParams struct {
+	ID         uuid.UUID
+	Expiration time.Time
+	Email      string
+	Valid      bool
+	Token      string
+}
+
+func (q *Queries) CreatePasswordReset(ctx context.Context, arg CreatePasswordResetParams) (PasswordReset, error) {
+	row := q.db.QueryRowContext(ctx, createPasswordReset,
+		arg.ID,
+		arg.Expiration,
+		arg.Email,
+		arg.Valid,
+		arg.Token,
+	)
+	var i PasswordReset
+	err := row.Scan(
+		&i.ID,
+		&i.Expiration,
+		&i.Email,
+		&i.Valid,
+		&i.Token,
+	)
+	return i, err
+}
+
 const createUser = `-- name: CreateUser :one
 INSERT INTO users(id,created_at,updated_at,name, api_key,email,passhash)
 VALUES($1,$2,$3,$4,
@@ -86,6 +119,23 @@ func (q *Queries) GetUserByEmail(ctx context.Context, email string) (User, error
 		&i.Email,
 		&i.Passhash,
 		&i.ApiKey,
+	)
+	return i, err
+}
+
+const resetPassword = `-- name: ResetPassword :one
+SELECT id, expiration, email, valid, token FROM password_resets WHERE token = $1
+`
+
+func (q *Queries) ResetPassword(ctx context.Context, token string) (PasswordReset, error) {
+	row := q.db.QueryRowContext(ctx, resetPassword, token)
+	var i PasswordReset
+	err := row.Scan(
+		&i.ID,
+		&i.Expiration,
+		&i.Email,
+		&i.Valid,
+		&i.Token,
 	)
 	return i, err
 }
